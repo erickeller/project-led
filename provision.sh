587aa1eac15ffd190de67a840a1634b5f2ef415f
Eric Keller <keller.eric@gmail.com>
2016-02-28 14:39:47 +0100

    create etcd* link to /usr/local/bin

--- provision.sh
+++ provision.sh
@@ -5,3 +5,5 @@ apt-get dist-upgrade -y >/dev/null 2>&1
 apt-get install -y curl >/dev/null 2>&1
 curl -L  https://github.com/coreos/etcd/releases/download/v2.3.0-alpha.1/etcd-v2.3.0-alpha.1-linux-amd64.tar.gz -o etcd-v2.3.0-alpha.1-linux-amd64.tar.gz
 tar xzvf etcd-v2.3.0-alpha.1-linux-amd64.tar.gz -C /opt/
+ln -sf /opt/etcd-v2.3.0-alpha.1-linux-amd64/etcd /usr/local/bin/etcd
+ln -sf /opt/etcd-v2.3.0-alpha.1-linux-amd64/etcdctl /usr/local/bin/etcdctl
