6a27431ee047017fb635de5825b1e29a7799984a
Eric Keller <keller.eric@gmail.com>
2016-03-12 18:58:37 +0100

    specify every master key to copy

--- master.sh
+++ master.sh
@@ -3,7 +3,9 @@
 apt-get install salt-master -y
 sudo sed -i 's/#interface: 0.0.0.0/interface: 0.0.0.0/' /etc/salt/master
 sudo sed -i 's/#auto_accept: False/auto_accept: True/' /etc/salt/master
-cp -f /vagrant/files/etc/salt/pki/master/master.p* /etc/salt/pki/master/
+mkdir -p /etc/salt/pki/master/
+cp -f /vagrant/files/etc/salt/pki/master/master.pub /etc/salt/pki/master/master.pub
+cp -f /vagrant/files/etc/salt/pki/master/master.pem /etc/salt/pki/master/master.pem
 # setting the right permissions to the keys
 chmod 400 /etc/salt/pki/master/master.pem
 chmod 644 /etc/salt/pki/master/master.pub
